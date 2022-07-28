cd3d1401f4cc573e760fb3ecb80d6babc58689eb
Ryan Collins <ryanjcollins41@gmail.com>
2022-07-28 12:59:34 -0400

    cleaned up formatting

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -17,8 +17,8 @@ function Home(props) {
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />{allData.map((business)=>{
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+          {allData.map((business)=>{
             return (
               <Marker position={business.coordinates}>
                 <Popup>
